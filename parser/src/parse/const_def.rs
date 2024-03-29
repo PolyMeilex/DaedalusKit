70d43510849732cdd2da4b4dd6733a9bc03fda80
PolyMeilex <marynczakbartlomiej@gmail.com>
2024-03-29 23:58:52 +0100

    [parser.const] Value and Array initializers expressions

--- parser/src/parse/const_def.rs
+++ parser/src/parse/const_def.rs
@@ -8,11 +8,21 @@ use std::fmt::Write;
 use super::{Expr, Ty};
 
 #[derive(Debug)]
+pub enum ConstKind<'a> {
+    Value {
+        init: Option<Expr<'a>>,
+    },
+    Array {
+        size_init: Expr<'a>,
+        init: Option<Vec<Expr<'a>>>,
+    },
+}
+
+#[derive(Debug)]
 pub struct Const<'a> {
     pub ident: &'a str,
     pub ty: Ty<'a>,
-    pub arr: Option<Expr<'a>>,
-    pub expr: Option<Expr<'a>>,
+    pub kind: ConstKind<'a>,
 }
 
 impl<'a> DaedalusDisplay for Const<'a> {
@@ -23,15 +33,26 @@ impl<'a> DaedalusDisplay for Const<'a> {
         self.ty.fmt(f)?;
         write!(f, " {}", self.ident)?;
 
-        if let Some(arr) = self.arr.as_ref() {
-            write!(f, "[")?;
-            arr.fmt(f)?;
-            write!(f, "]")?;
-        }
+        match &self.kind {
+            ConstKind::Value { init: Some(init) } => {
+                write!(f, " = ")?;
+                init.fmt(f)?;
+            }
+            ConstKind::Array { size_init, init } => {
+                write!(f, "[")?;
+                size_init.fmt(f)?;
+                write!(f, "]")?;
 
-        if let Some(expr) = self.expr.as_ref() {
-            write!(f, " = ")?;
-            expr.fmt(f)?;
+                if let Some(init) = init {
+                    write!(f, " = {{")?;
+                    for expr in init {
+                        expr.fmt(f)?;
+                        write!(f, ", ")?;
+                    }
+                    write!(f, "}}")?;
+                }
+            }
+            _ => {}
         }
 
         Ok(())
@@ -45,27 +66,47 @@ impl<'a> Const<'a> {
         let ty = Ty::parse(lexer)?;
         let ident = lexer.eat_token(Token::Ident)?;
 
-        let arr = if lexer.peek()? == Token::OpenBracket {
+        let kind = if lexer.peek()? == Token::OpenBracket {
             lexer.eat_token(Token::OpenBracket)?;
-            let index = Expr::parse(lexer)?;
+            let size_init = Expr::parse(lexer)?;
             lexer.eat_token(Token::CloseBracket)?;
-            Some(index)
+
+            let init = if lexer.peek()? == Token::Eq {
+                lexer.eat_token(Token::Eq)?;
+                lexer.eat_token(Token::OpenBrace)?;
+
+                let mut inits = Vec::new();
+
+                loop {
+                    let init = Expr::parse(lexer)?;
+                    inits.push(init);
+
+                    if lexer.peek()? == Token::CloseBrace {
+                        lexer.eat_token(Token::CloseBrace)?;
+                        break;
+                    } else {
+                        // This is not the last element comma is mandatory
+                        lexer.eat_token(Token::Comma)?;
+                    }
+                }
+
+                Some(inits)
+            } else {
+                None
+            };
+
+            ConstKind::Array { size_init, init }
         } else {
-            None
+            let init = if lexer.peek()? == Token::Eq {
+                lexer.eat_token(Token::Eq)?;
+                Some(Expr::parse(lexer)?)
+            } else {
+                None
+            };
+
+            ConstKind::Value { init }
         };
 
-        let expr = if lexer.peek()? == Token::Eq {
-            lexer.eat_token(Token::Eq)?;
-            Some(Expr::parse(lexer)?)
-        } else {
-            None
-        };
-
-        Ok(Self {
-            ident,
-            ty,
-            arr,
-            expr,
-        })
+        Ok(Self { ident, ty, kind })
     }
 }
